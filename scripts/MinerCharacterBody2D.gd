3e9dbf319009811c4860144fe88a73694620a222
Brad Barrows <bradley.barrows@pluto.tv>
2024-06-28 18:31:10 -0700

    Added JEnny

--- scripts/MinerCharacterBody2D.gd
+++ scripts/MinerCharacterBody2D.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
 
-@onready var sprite: AnimatedSprite2D = $MinerAnimatedSprite2D
+@onready var sprite: AnimatedSprite2D = $JennyAnimatedSprite2D
 @onready var gunNode: Node2D = $GunNode2D
 @onready var centerNode: Node2D = $CenterNode2D
 
@@ -19,13 +19,15 @@ var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # FOR TOUCH SIM
 var touch_start_position: Vector2
+var touch_start_dist: float = 0.0
+
 #var is_touching: bool = false
 var is_long_pressing: bool = false
 var in_air: bool = false
 var touch_started: bool = false
 
 # Thresholds
-const TAP_DRAG_THRESHOLD: float = 20.0
+const TAP_DRAG_THRESHOLD: float = 80.0
 
 
 func _unhandled_input(event):
@@ -42,6 +44,7 @@ func _unhandled_input(event):
 func _touch_began(event_position: Vector2):
 	touch_started = true
 	touch_start_position = event_position
+	touch_start_dist = touch_start_position.distance_to(get_global_mouse_position())
 	start_global_position = self.global_position
 	print("touch_start_position ", touch_start_position, "    global pos  ", self.global_position)
 	
@@ -91,6 +94,14 @@ func _ready() -> void:
 	original_position = sprite.position
 	
 func _physics_process(delta: float) -> void:
+	
+	if touch_started:
+		var touch_dist = touch_start_position.distance_to(get_global_mouse_position())
+		#print("touch_start_dist ", touch_start_dist, " touch dist   ", touch_dist)
+		var touch_dist_diff = abs(touch_start_dist - touch_dist)
+		print("touch diff ", touch_dist_diff)
+		if touch_dist_diff > 5.0:
+			velocity.x = 0
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -107,11 +118,12 @@ func _physics_process(delta: float) -> void:
 		var toMouseVector = centerNode.global_position.direction_to(mp)
 		
 		var angle = rad_to_deg(normalGunVector.angle_to(toMouseVector))
+		print("angle ", angle)
 		gunNode.rotation_degrees = angle
 		if (angle < -90 and angle > -180) or (angle < 180 and angle > 90):
-			sprite.flip_h = true
-		else:
 			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
 
 	var direction = 1
 	move_and_slide()
