a0f59a3a5b677223da0c92190e88f16269f40fc1
Brad Barrows <bradley.barrows@pluto.tv>
2024-06-28 19:42:05 -0700

    Water falls down

--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -21,6 +21,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var new_water_tiles = []
 	for water_tile_coord in water_tiles:
 		var atlas_water_for_current_coords = self.get_cell_atlas_coords(0, water_tile_coord)
 		var tilemap_below_coords = self.get_neighbor_cell(water_tile_coord, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE)
@@ -28,6 +29,10 @@ func _process(delta: float) -> void:
 		#print("atlas_below_water_coords ", atlas_below_water_coords)
 		if atlas_below_water_coords in GameManager.EMPTY_CELLS:
 			#print("BEB tilemap_below_coords", tilemap_below_coords, "has empty cell")
+			new_water_tiles.push_back(tilemap_below_coords)
 			self.set_cell(0, tilemap_below_coords, 1, GameManager.DEFAULT_WATER_CELL)
-		#var tile_coords_for_bc = self.get_cell_atlas_coords(0, atlas_below_water_coords)
+			self.set_cell(0, water_tile_coord, 1, GameManager.DEFAULT_EMPTY_CELL)
+		else:
+			new_water_tiles.push_back(water_tile_coord)
+	water_tiles = new_water_tiles
 	pass
